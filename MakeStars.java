10239c9ec74985daf01ea6ef2cb90d78975f6f71
Justin <justin82049@gmail.com>
2021-01-04 22:56:43 -0500

    first test

--- MakeStars.java
+++ MakeStars.java
@@ -1,4 +1,12 @@
 import java.util.*;
 public class MakeStars{
   public static void main(String[]args ){
+    Scanner n = new Scanner(System.in);
+    while(n.hasNextLine()){
+      Scanner line = new Scanner(n.nextLine());
+      while (line.hasNext()){
+        System.out.println("*")
+}
+
+}
 }
