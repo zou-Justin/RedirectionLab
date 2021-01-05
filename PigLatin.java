09d3ccc878c4115279bea5c5030112e5a6f74e82
Justin <justin82049@gmail.com>
2021-01-04 23:47:41 -0500

    somewhat working for best

--- PigLatin.java
+++ PigLatin.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class PigLatin{
+
   public static String pigLatinSimple(String s){
     String[] vowels = new String[]{"a","e","i","o","u"};
     for (int i = 0; i < vowels.length;i++){
@@ -11,23 +12,38 @@ public class PigLatin{
     }
     s = s.substring(1,s.length()) + s.substring(0,1) + "ay";
     return s.toLowerCase();
+  }
+  public static void main(String[]args){
+  //System.out.println(pigLatinBest("science"));
+  // System.out.println(pigLatinBest("4chan"));
+  // System.out.println(pigLatinBest("fish!"));
+  // System.out.println(pigLatinBest("fish") );
+  // System.out.println(pigLatinBest("the."));
+  // System.out.println(pigLatinBest("cat!"));
+  // System.out.println(pigLatinBest("amazing?")	);
+  // System.out.println(pigLatinBest("apple%"));
+   Scanner n = new Scanner(System.in);
+  while(n.hasNext()){
+      String line = n.nextLine();
+      String stuff = "";
+      String content = n.next();
+      for (int i = 0; i < line.length();i++){
+        stuff += pigLatinBest(content);
+      }
+      System.out.println(stuff);
+  }
 }
-public static void main(String[]args){
-  System.out.println(pigLatin("the"));
-  System.out.println(pigLatin("skee"));
-  System.out.println(pigLatin("emu"));
-  System.out.println(pigLatin("grade"));
-}
+
+
   public static String pigLatin(String s){
-    String[] vowels = new String[]{"a","e","i","o","u"};
     String[] diagraphs = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
     for (int i = 0; i < diagraphs.length;i++){
         if (s.substring(0,2).equals(diagraphs[i])){
-          s += s.substring(0,2) + "ay";
-          s = s.substring(2,s.length());
+          s = s.substring(2,s.length()) + s.substring(0,2) + "ay";
           return s.toLowerCase();
         }
     }
+    String[] vowels = new String[]{"a","e","i","o","u"};
     for (int i = 0; i < vowels.length;i++){
         if (s.substring(0,1).equals(vowels[i])){
           s += "hay";
@@ -38,8 +54,26 @@ public static void main(String[]args){
     return s.toLowerCase();
   }
   public static String pigLatinBest(String s){
-    String[] Alphabet = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-    String[] AlphabetNumbers = new String[]{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"};
+    String[] Alphabet = new String[]{"a","b","c","C","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    String[] NumberAlphabet = new String[]{"1","2","3","4","5","6","7","8","9","0","a","b","c","C","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+    for (int i = 0; i < Alphabet.length;i++){
+      if (s.substring(0,1).toLowerCase().equals(Alphabet[i])){
+        String b = "";
+        for (int j = 0;j < NumberAlphabet.length;j++){
+          if (!s.substring(s.length()-1,s.length()).toLowerCase().equals(NumberAlphabet[j])){
+            b = pigLatin(s.substring(0,s.length()-1));
+            b += s.substring(s.length()-1,s.length());
+          }
+          else if(s.substring(s.length()-1,s.length()).toLowerCase().equals(NumberAlphabet[j])){
+            return pigLatin(s);
+          }
+        }
+        return b.toLowerCase();
+      }
+
+    }
     
+    return s.toLowerCase();
   }
 }
