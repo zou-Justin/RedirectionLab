326dc182b55f8194d1889a1ab79647f2c4435078
Justin <justin82049@gmail.com>
2021-01-04 23:36:51 -0500

    looped through each word and prevented the infinite loop

--- MakeStars.java
+++ MakeStars.java
@@ -1,12 +1,18 @@
 import java.util.*;
 public class MakeStars{
   public static void main(String[]args ){
-    Scanner n = new Scanner(System.in);
-    while(n.hasNextLine()){
-      Scanner line = new Scanner(n.nextLine());
-      while (line.hasNext()){
-        System.out.println("*")
-}
+      Scanner n = new Scanner(System.in);
+        while(n.hasNextLine()){
+            Scanner line = new Scanner(n.nextLine());
+            while (line.hasNext()){
+              String star = line.next();
+              for (int i = 0;i < star.length();i++){
+                System.out.print("*");
+              }
+              System.out.print(" ");
+            }
+          System.out.println();
+        }
 
 }
 }
