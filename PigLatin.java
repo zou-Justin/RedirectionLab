0287384e497b87bd78b3b7e7fa6ec554032c9722
Justin <justin82049@gmail.com>
2021-01-04 23:12:20 -0500

    pigLatin normal, added diagraphs and copied paste from simple

--- PigLatin.java
+++ PigLatin.java
@@ -18,5 +18,23 @@ public static void main(String[]args){
   System.out.println(pigLatinSimple("david"));
   System.out.println(pigLatinSimple("aaron"));
 }
-
+  public static String pigLatin(String s){
+    String[] vowels = new String[]{"a","e","i","o","u"};
+    String[] diagraphs = new String[]{"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    for (int i = 0; i < diagraphs.length;i++){
+        if (s.substring(0,2).equals(diagraphs[i])){
+          s += s.substring(0,2) + "ay";
+          s = s.substring(2,s.length());
+          return s.toLowerCase();
+        }
+    }
+    for (int i = 0; i < vowels.length;i++){
+        if (s.substring(0,1).equals(vowels[i])){
+          s += "hay";
+          return s.toLowerCase();
+        }
+    }
+    s = s.substring(1,s.length()) + s.substring(0,1) + "ay";
+    return s.toLowerCase();
+  }
 }
