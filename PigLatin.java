6bcf2b87357f061c221acdb1ae5677152225a9e8
Justin <justin82049@gmail.com>
2021-01-04 23:04:33 -0500

    simple Code

--- PigLatin.java
+++ PigLatin.java
@@ -2,8 +2,13 @@ import java.util.*;
 
 public class PigLatin{
   public static String pigLatinSimple(String s){
-
-
+    String[] vowels = new String[]{"a","e","i","o","u"};
+    for (int i = 0; i < vowels.length;i++){
+        if (s.substring(0,1).equals(vowels[i])){
+          s += "hay";
+          return s.toLowerCase();
+        }
+    }
 }
 
 }
